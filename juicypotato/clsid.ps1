943119ff6adfc0988a4984f27c5fd0f3ebb7166d
O2C14 <70356077+O2C14@users.noreply.github.com>
2023-04-05 19:18:50 +0800

    Delete juicypotato directory

--- juicypotato/clsid.ps1
+++
@@ -1,6 +0,0 @@
-New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
-$CLSID = Get-ItemProperty HKCR:\clsid\* | select-object AppID,@{N='CLSID'; E={$_.pschildname}} | where-object {$_.appid -ne $null}
-foreach($a in $CLSID)
-{
-	Write-Host $a.CLSID
-}
